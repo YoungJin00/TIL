56578a5dde0d7ce1f42b2fbf9b7037eff1ee14fa
YoungJin00 <dud_wls_95@naver.com>
2023-07-25 23:38:14 +0900

    plus

--- algorithm/백준/5086.py
+++ algorithm/백준/5086.py
@@ -1,12 +1,17 @@
 # 배수와 약수
-# a=1
-# while a:
-#     b, c = map(int, input().split())
-#     if b >= c:
-#         if b % c == 0:
-#             print('multiple')
-#         else:
-#             print('neither')
-#     elif b <= c:
-#         if           
+while 1:
+    b, c = map(int, input().split())
+    if b == 0 and c == 0:
+        break
+    if b >= c:
+        if b % c == 0:
+            print('multiple')
+        else:
+            print('neither')
+    elif b <= c:
+        if c % b == 0:
+            print('factor')
+    
+    
+               
          
