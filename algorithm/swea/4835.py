40dbe24b0f39ac8261f6067cfdd0245ffd40924c
YoungJin00 <dud_wls_95@naver.com>
2023-08-01 17:41:45 +0900

    0801c

---
+++ algorithm/swea/4835.py
@@ -0,0 +1,14 @@
+# 구간합 16101
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    li = list(map(int, input().split()))
+    max1 = sum(li[0:M])
+    min1 = sum(li[0:M])
+    for i in range(N-M+1):
+        res = sum(li[i:i+M])
+        if res > max1:
+            max1 = res
+        elif res < min1:
+            min1 = res
+    print(f'#{tc} {max1 - min1}')
--- algorithm/swea/swea1206.py
+++ algorithm/swea/swea1206.py
@@ -10,23 +10,18 @@ for test_case in range(1,11):
         if d_2 > 0 and d_1 > 0 and d1 > 0 and d2 > 0:
           res += min(d_2, d_1, d1, d2)
     print(f'#{test_case} {res}')
-    
-    
-"""
-T = 10                      # TC 10개 고정
-for tc in range(1, T+1):    #
-    N = int(input())        # 가로 길이
-    height = list(map(int,input().split()))
 
-    view = 0
-    for i in range(2, N-2):
-        max_v = height[i-2]
-        if max_v < height[i-1]:
-            max_v = height[i-1]
-        if max_v < height [i+2]:
-            max_v = height[i+2]
-        if height[i] > max_v :  # i가 주변 보다 높으면
-            view += height[i] - max_v
 
-    print(f'#{tc} view')
-"""    
+
+# for tc in range(1, 11):
+#     N = int(input())
+#     height = list(map(int, input().split()))
+
+#     view = 0
+#     for i in range(2, N-2):
+#         max_v = max(height[i-2], height[i-1], height[i+1], height[i+2])
+#         if height[i] > max_v:
+#             view += height[i] - max_v
+
+#     print(f'#{tc} {view}')
+
