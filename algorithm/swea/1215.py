c575ece9cc44fafb8633aa5425c37d62d700eaeb
YoungJin00 <dud_wls_95@naver.com>
2023-08-07 17:39:42 +0900

    0807

---
+++ algorithm/swea/1215.py
@@ -0,0 +1,44 @@
+# 회문 1
+
+for tc in range(1, 11):
+    N = int(input())
+    MAP = [input() for _ in range(8)]
+    # print(MAP)
+
+    # 가로
+    row_cnt = 0
+    for i in range(8):
+        for j in range(8-N+1):
+            s = MAP[i][j:j+N]
+            if s == s[::-1]:
+                row_cnt += 1
+    # print(row_cnt)
+
+    # 세로
+    li = []
+    for j in range(8 -N +1):
+        for i in range(8):
+            s = ''.join(MAP[j+x][i] for x in range(N))
+            li.append(s)
+    col_cnt = 0
+    for i in range(len(li)):
+        if li[i] == li[i][::-1]:
+            col_cnt += 1
+
+    res = row_cnt + col_cnt
+    print(f'#{tc} {res}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
