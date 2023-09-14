8b0db040041551955d14dfe07d4e9156e6510938
YoungJin00 <dud_wls_95@naver.com>
2023-09-14 17:53:01 +0900

    0914

--- algorithm/백준/24479.py
+++ algorithm/백준/24479.py
@@ -1,6 +1,8 @@
 # 알고리즘 수업 - 깊이 우선 탐색 1
 import sys
-sys.setrecursionlimit(15000000)
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
 
 def dfs(r):
     global cnt
@@ -11,11 +13,12 @@ def dfs(r):
             cnt += 1
             dfs(i)
 
+
 N, M, R = map(int, input().split())
 cnt = 1
 visited = [0] * (N+1)
-g = [ [] for _ in range(N+1)]
-for _ in range(N):
+g = [[] for _ in range(N+1)]
+for _ in range(M):
     a, b = map(int, input().split())
     g[a].append(b)
     g[b].append(a)
