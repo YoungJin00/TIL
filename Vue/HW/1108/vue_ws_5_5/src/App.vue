ec9e6f2d1e619678bcf97fc191b571949d37bcdc
YoungJin00 <dud_wls_95@naver.com>
2023-11-08 21:14:47 +0900

    1108+

---
+++ Vue/HW/1108/vue_ws_5_5/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="container">
+      <header class="header">
+        <h1>명함 관리 페이지</h1>
+      </header>
+      <main class="main">
+        <p>명함을 관리하는 페이지입니다. 여기에 명함 목록이 표시됩니다.</p>
+        <h1>보유 명함 목록</h1>
+  
+      </main>
+      <article>
+        <CreateCardForm @create-card-event="updateCard"/>
+        <BusinessCard :newCard="newCard" />
+      </article>
+      <footer class="footer">
+        <p>&copy;2023 My Business Cards</p>
+      </footer>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import BusinessCard from '@/components/BusinessCard.vue'
+  import CreateCardForm from '@/components/CreateCardForm.vue'
+  const newCard = ref({})
+  const updateCard = function (card) {
+    newCard.value = card
+  }
+
+  </script>
+  
+  <style scoped>
+  * {
+    text-align: center;
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .header {
+    background-color: rgb(56, 130, 228);
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+  }
+  h1 {
+    text-align: center;
+  }
+  
+  .main {
+    padding: 20px;
+  }
+  
+  .footer {
+    background-color: grey;
+    text-align: center;
+    width: 100%;
+    position: fixed;
+    bottom: 0;
+  }
+  </style>
+  
---
+++ Vue/HW/1108/vue_ws_5_5/src/components/CreateCardForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <form>
+            <label for="name">이름 : </label>
+            <input type="text" id="name" v-model="name" name="name">
+            <label for="title">직함 : </label>
+            <input type="text" id="title" v-model="title" name="title">
+            <button @click.prevent="createCard">명함 추가</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const name = ref('')
+const title = ref('')
+const emit = defineEmits(['createCardEvent'])
+const createCard = function () {
+    const card = {
+        name: name.value,
+        title: title.value
+    }   
+    emit('createCardEvent', card)
+    name.value = ''
+    title.value = ''
+}
+
+</script>
+
+<style scoped>
+
+</style>
