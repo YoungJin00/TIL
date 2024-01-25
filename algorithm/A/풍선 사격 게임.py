921227c33283d5949c97c9e5f9d7ab7d85b4924d
YoungJin00 <dud_wls_95@naver.com>
2024-01-25 23:51:21 +0900

    add: algorithm sol

--- algorithm/A/풍선 사격 게임.py
+++ algorithm/A/풍선 사격 게임.py
@@ -39,3 +39,62 @@ for t in range(1, T+1):
         result = calScore(balloons)
 
     print(f'#{t} {result}')
+    
+    
+    
+# 2
+T = int(input())
+
+for tc in range(1, T + 1):
+
+    N = int(input())
+
+    balloon_lst = list(map(int, input().split()))
+
+    max_score = 0
+
+    def find_score(lst, score):
+        # global 변수
+        global max_score
+
+        # 백트래킹
+        tmp_total = 0
+        tmp = len(lst) - 2
+        if tmp > 0:
+            tmp_total += (1000 ** 2) * tmp
+            tmp_total += 2000
+        else:
+            tmp_total += len(lst) * 1000
+
+        if score + tmp_total <= max_score:
+            return
+
+        # 종료조건
+        if len(lst) == 0:
+            max_score = max(score, max_score)
+            return
+
+        # 실행문
+        if len(lst) == 1:
+            tmp = lst.pop()
+            find_score(lst, score + tmp)
+            lst.append(tmp)
+
+        else:
+            for i in range(len(lst)):
+                if i == 0:
+                    tmp = lst.pop(0)
+                    find_score(lst, score + lst[0])
+                    lst.insert(0, tmp)
+                elif i == len(lst) - 1:
+                    tmp = lst.pop()
+                    find_score(lst, score + lst[i - 1])
+                    lst.append(tmp)
+                else:
+                    tmp = lst.pop(i)
+                    find_score(lst, score + (lst[i - 1] * lst[i]))
+                    lst.insert(i, tmp)
+
+    find_score(balloon_lst, 0)
+
+    print(f"#{tc} {max_score}")
